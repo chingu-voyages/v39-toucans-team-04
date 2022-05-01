e49088e2f5227e8b02ed6c23e0adf86fe2f9b2aa
snowlizard <illegal_burrito@live.com>
2022-04-30 19:32:43 -0700

    removed unneccessary lines of code

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,6 @@ import categoryReducer from './categoryreducer';
 import difficultyReducer from './difficultyreducer';
 import { configureStore } from '@reduxjs/toolkit';
 
-import { combineReducers, compose, applyMiddleware } from 'redux';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = configureStore({
     reducer: {
         category: categoryReducer,
