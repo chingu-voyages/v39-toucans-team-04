c345f7013c2cd1adc06d187c2b82045b11928e0f
snowlizard <illegal_burrito@live.com>
2022-05-10 16:51:43 -0700

    added code for import tech photos

---
+++ src/components/mixins/tech.js
@@ -0,0 +1,16 @@
+import one from '../../assets/images/tech/01.png'
+import two from '../../assets/images/tech/02.jpg'
+import three from '../../assets/images/tech/03.jpg'
+import four from '../../assets/images/tech/04.jpg'
+import five from '../../assets/images/tech/05.jpg'
+import six from '../../assets/images/tech/06.jpg'
+import seven from '../../assets/images/tech/07.png'
+import eight from '../../assets/images/tech/08.jpg'
+import nine from '../../assets/images/tech/09.jpeg'
+import ten from '../../assets/images/tech/10.jpg'
+
+
+export const techPics = [
+    one, two, three, four, five,
+    six, seven, eight, nine, ten
+];
