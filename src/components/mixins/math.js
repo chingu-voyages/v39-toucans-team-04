6a6e41f58d7477ec638969c00e27578f87373eba
snowlizard <illegal_burrito@live.com>
2022-05-10 16:59:12 -0700

    added math and science pictures arrays

---
+++ src/components/mixins/math.js
@@ -0,0 +1,15 @@
+import one from '../../assets/images/math/01.png'
+import two from '../../assets/images/math/02.jpg'
+import three from '../../assets/images/math/03.jpg'
+import four from '../../assets/images/math/04.jpg'
+import five from '../../assets/images/math/05.jpg'
+import six from '../../assets/images/math/06.jpg'
+import seven from '../../assets/images/math/07.png'
+import eight from '../../assets/images/math/08.jpg'
+import nine from '../../assets/images/math/09.jpg'
+import ten from '../../assets/images/math/10.jpg'
+
+export const mathPics = [
+    one, two, three, four, five,
+    six, seven, eight, nine, ten
+];
---
+++ src/components/mixins/science.js
@@ -0,0 +1,13 @@
+import one from '../../assets/images/science/01.png'
+import two from '../../assets/images/science/02.jpg'
+import three from '../../assets/images/science/03.jpg'
+import four from '../../assets/images/science/04.jpg'
+import five from '../../assets/images/science/05.jpg'
+import six from '../../assets/images/science/06.jpg'
+import seven from '../../assets/images/science/07.jpg'
+import eight from '../../assets/images/science/08.jpg'
+
+export const sciencePics = [
+    one, two, three, four, five,
+    six, seven, eight
+];
